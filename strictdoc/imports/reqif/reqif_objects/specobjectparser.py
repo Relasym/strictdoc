bf5bf4d31a021c2ef851739f587bbf91eb8bc74e
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-10-21 11:09:42 +0200

    edit specobjectparser

--- strictdoc/imports/reqif/reqif_objects/specobjectparser.py
+++ strictdoc/imports/reqif/reqif_objects/specobjectparser.py
@@ -39,25 +39,52 @@ class SpecObjectParser:
     # def parse(element, type, attributes_map, relation_map):
 
     @staticmethod
-    def parse(element):
+    def parse(element, attribute_map):
         # append all values to an empty list
         attribute_list = []
         for elem in element[0]:
             attribute_list.append(elem.attrib["THE-VALUE"])
         print(attribute_list)
 
-        # append all identifiers to an empty list
+        # append all identifiers from the spec object to an empty list
         identifier_list = []
-        for elem in element[0]:
-            identifier_list.append(elem[0][0].text)
+        for identifier in element[0]:
+            identifier_list.append(identifier[0][0].text)
         print(identifier_list)
 
-        # MISSING - check for missing or malformed
+        """check if all identifiers from attibute_map(from spec object type)
+        are in identifier_list(from spec object) and reversed
+        if not, raise ValueError"""
+        identifier_counter = 0
+        for identifier in identifier_list:
+            for key in attribute_map:
+                if identifier == key:
+                    identifier_counter += 1
+
+        if len(identifier_list) != identifier_counter or len(attribute_map) != identifier_counter:
+            raise ValueError("SpecObjectIdentifiers not congruent")
+
+        # print(identifier_list)
 
         # MISSING - dict with attribute names and corresponding identifiers
+        """
         dict_attr = {}
         for identifier in identifier_list:
-            dict_attr.update()
+            for attribute in attribute_map:
+                print(attribute_map.get(attribute))
+        """
+        # MISSING - check for missing or malformed (type : in technical relation required, in functional not
+        # MISSING - initialize requirement object and assign values
 
-        # initialize requirement object and assign values
+        """for(attributes):
+            get values;
+            specobject.value = value
+        check attributes;
+        if technical attributes -> specobject.type = technical
+        if functional attr...;
+        if other attr or attr missing -> parse_all
+        """
+        # raise ValueError("uid_malformed")
+        return Requirement("Type", "UID", "Content", "Status", 'status', 'tags', 'references', 'title', 'body',
+                           'rationale', 'rationale_multiline', 'comments', 'special_fields')
 
