7d4a72324e6c2e14670e376e2f701332dd9855b4
Barrazal <maxi1010@gmx.at>
2021-10-21 13:54:33 +0200

    Example of specrelationparser.py

--- strictdoc/imports/reqif/reqif_objects/specrelationparser.py
+++ strictdoc/imports/reqif/reqif_objects/specrelationparser.py
@@ -1,6 +1,18 @@
+# Todo: Implement ParserFunction
+from xml.etree import ElementTree as etree
+from xml.etree.ElementTree import Element
+
 class SpecRelationParser:
     """currently no method"""
 
     @staticmethod
-    def parse():
+    def parse(element):
+        # ToDo: No definition of Target and Source!!
         """I can´t do anything!"""
+        with open(input_file, "r", encoding="UTF-8") as file:
+            # Read each line in the file, readlines() returns a list of lines
+            content = file.read()
+        try:
+            parsed_xml = etree.parse(StringIO(content), etree.XMLParser())
+        except Exception as e:
+            assert 0
