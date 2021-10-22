7210e577949d0cf0aefc477993c633186fea32f2
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-10-22 08:40:28 +0200

    Update specobjectparser.py

--- strictdoc/imports/reqif/reqif_objects/specobjectparser.py
+++ strictdoc/imports/reqif/reqif_objects/specobjectparser.py
@@ -39,12 +39,15 @@ class SpecObjectParser:
     # def parse(element, type, attributes_map, relation_map):
 
     @staticmethod
-    def parse(element, attribute_map):
-        # append all values to an empty list
-        attribute_list = []
+    def parse(element, type, attribute_map):
+        # check if value contains ' or "; if yes, raise an error; if no, append to list
+        value_list = []
         for elem in element[0]:
-            attribute_list.append(elem.attrib["THE-VALUE"])
-        print(attribute_list)
+            for char in elem:
+                if char == '"' or char == "'" or char == "/":
+                    raise ValueError("Attribute_contains_illegal_character")
+            value_list.append(elem.attrib["THE-VALUE"])
+        print(value_list)
 
         # append all identifiers from the spec object to an empty list
         identifier_list = []
@@ -52,32 +55,51 @@ class SpecObjectParser:
             identifier_list.append(identifier[0][0].text)
         print(identifier_list)
 
-        """check if all identifiers from attibute_map(from spec object type)
-        are in identifier_list(from spec object) and reversed
-        if not, raise ValueError"""
+        # check if all identifiers from attibute_map(from spec object type)
+        # are in identifier_list(from spec object) and reversed
+        # if not, raise ValueError
         identifier_counter = 0
         for identifier in identifier_list:
             for key in attribute_map:
                 if identifier == key:
                     identifier_counter += 1
-
         if len(identifier_list) != identifier_counter or len(attribute_map) != identifier_counter:
             raise ValueError("SpecObject_Identifiers_not_congruent")
 
-
-
-
-        # print(identifier_list)
-
-        # MISSING - dict with attribute names and corresponding identifiers
-        """
-        dict_attr = {}
+        # dict with attribute names and corresponding identifiers
+        value_number = 0
+        dict_attribute_value = {}
         for identifier in identifier_list:
             for attribute in attribute_map:
-                print(attribute_map.get(attribute))
-        """
-        # MISSING - check for missing or malformed (type : in technical relation required, in functional not
+                if identifier == attribute:
+                    dict_attribute_value[attribute_map[attribute]] = value_list[value_number]
+                    value_number += 1
+        print(dict_attribute_value)
+
+        # MISSING - check for missing
         # MISSING - initialize requirement object and assign values
+
+        # UserWarning or ValueError??
+
+        if type == "test":
+            for attribute in dict_attribute_value:
+                if dict_attribute_value[attribute] == "":
+                    if attribute != "status":
+                        raise UserWarning
+
+        if type == "technical":
+            for attribute in dict_attribute_value:
+                if dict_attribute_value[attribute] == "":
+                    if attribute == "status" or attribute == "relation" or attribute == "technical_description":
+                        raise UserWarning
+                    # if  format raise exc
+
+        if type == "technical":
+            for attribute in dict_attribute_value:
+                if dict_attribute_value[attribute] == "":
+                    if attribute == "status" or attribute == "technical_description":
+                        raise UserWarning
+                    # if  format raise exc
 
         """for(attributes):
             get values;
@@ -87,7 +109,7 @@ class SpecObjectParser:
         if functional attr...;
         if other attr or attr missing -> parse_all
         """
-        # raise ValueError("uid_malformed")
+
         return Requirement("Type", "UID", "Content", "Status", 'status', 'tags', 'references', 'title', 'body',
                            'rationale', 'rationale_multiline', 'comments', 'special_fields')
 
