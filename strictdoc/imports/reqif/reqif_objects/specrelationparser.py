8790f944cb9585a139654d94f6521c55ced7d4a0
Barrazal <maxi1010@gmx.at>
2021-10-27 14:06:56 +0200

    update and delete Files

--- strictdoc/imports/reqif/reqif_objects/specrelationparser.py
+++ strictdoc/imports/reqif/reqif_objects/specrelationparser.py
@@ -11,50 +11,13 @@ class SpecRelationParser:
     """currently no method"""
 
     @staticmethod
-    def parse(input_element):
+    def parse(specrelation):
         # ToDo: No definition of Target and Source!!
         """I can´t do anything!"""
-        with open(input_element, "r", encoding="UTF-8") as file:
-            # Read each line in the file, readlines() returns a list of lines
-            content = file.read()
-        try:
-            parsed_xml = eTree.parse(StringIO(content), eTree.XMLParser())
-        except Exception as e:
-            assert 0
+        relation_map  = {}
 
-        parsed_spec_objects = []
-
-        # https://stackoverflow.com/a/12946675/598057
-        def get_namespace(element):
-            m = re.match(r'\{.*\}', element.tag)
-            return m.group(0) if m else ''
-
-        top_level_reqif_element = parsed_xml.getroot()
-
-        namespace = get_namespace(top_level_reqif_element)
-        print(f"namespace: {namespace}")
-
-        print(f"top level: {top_level_reqif_element}")
-
-        top_level_reqif_element_children: List[Element] = list(top_level_reqif_element)
-        core_content_element = top_level_reqif_element_children[1]
-        print(f"CORE-CONTENT: {core_content_element}")
-
-        core_content_element_children = list(core_content_element)
-        reqif_content_element = core_content_element_children[0]
-        print(f"REQ-IF-CONTENT: {reqif_content_element}")
-
-        reqif_content_children = list(reqif_content_element)
-        spec_objects = reqif_content_children[2]
-        print(f"SPEC-OBJECTS: {spec_objects}")
-
-        spec_objects_children = list(spec_objects)
-        print(spec_objects_children[1])
-
-# Todo: Filepath hardcoded!
 # file = "../../../../tests/unit/strictdoc/import/reqif/mapping_testfile.reqif"
 
-# SpecRelationParser.parse(file)
 
 # https://www.geeksforgeeks.org/python-program-check-string-contains-special-character/
 #
@@ -65,5 +28,5 @@ class SpecRelationParser:
 #             source
 #             target
 #
-# Identifier contains "_ - . A-Z a-z 0-9"
+# for invalid ID: Identifier contains "_ - . A-Z a-z 0-9"
 
