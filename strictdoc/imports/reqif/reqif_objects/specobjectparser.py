f2dc81b0eeb1fb114f05252e003b44b8c9528f14
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-10-29 10:31:05 +0200

    Update specobjectparser.py

--- strictdoc/imports/reqif/reqif_objects/specobjectparser.py
+++ strictdoc/imports/reqif/reqif_objects/specobjectparser.py
@@ -1,4 +1,3 @@
-import time
 from xml.etree import ElementTree as etree
 
 from strictdoc.backend.dsl.models.requirement import Requirement
@@ -6,43 +5,16 @@ from strictdoc.backend.dsl.models.special_field import SpecialField
 
 
 class SpecObjectParser:
-    #   def __init__(self, test_type=None, uid=None, status=None, title=None, allocation=None,
-    #                asil=None, objective=None, initial_condition=None, test_sequence=None, target_value=None,
-    #                reference=None, allocation_to_component=None, comment=None, technical_description=None,
-    #                functional_description=None):
-    #       self.type = test_type
-    #       self.uid = uid
-    #       self.title = title
-    #       self.status = status
-    #       self.allocation = allocation
-    #       self.asil = asil
-    #       self.objective = objective
-    #       self.initial_condition = initial_condition
-    #       self.test_sequence = test_sequence
-    #       self.target_value = target_value
-    #       self.reference = reference
-    #       self.allocation_to_component = allocation_to_component
-    #       self.comment = comment
-    #       self.technical_description = technical_description
-    #       self.functional_description = functional_description
-    #
-    #   def __str__(self):
-    #       """returns string"""
-    #       return f"""SpecObject(type = {self.type}, uid = {self.uid}, title = {self.title}, status = {self.status},
-    #       allocation = {self.allocation}, asil = {self.asil}, objective = {self.objective},
-    #       initial_condition = {self.initial_condition}, test_sequence = {self.test_sequence},
-    #       target_value = {self.target_value}, reference = {self.reference},
-    #       allocation_to_component = {self.allocation_to_component}, comment = {self.comment},
-    #       technical_description = {self.technical_description},
-    #       functional_description = {self.functional_description})"""
-    #
-    #   def __repr__(self):
-    #       return str(self)
-
-    # def parse(element, element_type, attributes_map, relation_map):
 
     @staticmethod
-    def parse(element, element_type, attribute_map):
+    def parse(element, object_type_map, relation_map):
+
+        # get type and attribute map
+
+        element_type_identifier = element[1][0].text
+        element_type = object_type_map[element_type_identifier][0]
+        attribute_map = object_type_map[element_type_identifier][1]
+
         # check if value contains ' or "; if yes, raise an error; if no, append to list
         value_list = []
         for elem in element[0]:
@@ -59,7 +31,7 @@ class SpecObjectParser:
         # check if all identifiers from attibute_map(from spec object type)
         # are in identifier_list(from spec object) and reversed
         # if not, raise ValueError;
-        # ToDo make check of identifiers faster?
+        # ToDo make check of identifiers faster
         identifier_counter = 0
         for identifier in identifier_list:
             for key in attribute_map:
@@ -74,19 +46,17 @@ class SpecObjectParser:
         for identifier in identifier_list:
             dict_attribute_value[attribute_map[identifier]] = value_list[value_number]
             value_number += 1
-        print(dict_attribute_value)
-        # value_list        ['LLR001-T001', 'true'
-        # identifier_list   ['_BSKKIS2GEeyvlO4vtsM_UA', '_a5wPYC2GEeyvlO4vtsM_UA',
-        # attributes_map    {"_BSKKIS2GEeyvlO4vtsM_UA": "requirement_ID",
-        #                   "_BSKKJC2GEeyvlO4vtsM_UA": "type",
 
         # check if values in attribute asil are correct asil values
         asil_values = ("ASIL A", "ASIL B", "ASIL C", "ASIL D",
                        "ASIL-A", "ASIL-B", "ASIL-C", "ASIL-D",
-                       "A", "B", "C", "D", "QM")
+                       "A", "B", "C", "D", "QM", "")
         if element_type == "functional" or element_type == "technical":
             if dict_attribute_value["asil"] not in asil_values:
                 raise ValueError("Attribute_asil_contains_no_asil_value")
+
+        # get parents
+        list_parents = relation_map[dict_attribute_value["requirement_ID"]]
 
         # check for missing attribute values in predefined spectypes
         if element_type == "test":
@@ -94,18 +64,43 @@ class SpecObjectParser:
                 if dict_attribute_value[attribute] == "":
                     if attribute != "type" and attribute != "status":
                         raise ValueError("Required_attribute_missing")
-            # initialize requirement object and assign values
-            requirement = Requirement(uid=dict_attribute_value["requirement_ID"], title=dict_attribute_value["title"],
-                                      status=dict_attribute_value["status"], references=dict_attribute_value["traceability"],
-                                      )
+            # initialize special fields
+            special_field_type = SpecialField(dict_attribute_value["requirement_ID"], "type",
+                                              dict_attribute_value["type"])
+            special_field_initial_condition = SpecialField(dict_attribute_value["requirement_ID"], "initial_condition",
+                                                           dict_attribute_value["initial_condition"])
+            special_field_test_sequence = SpecialField(dict_attribute_value["requirement_ID"], "test_sequence",
+                                                       dict_attribute_value["test_sequence"])
+            special_field_target_value = SpecialField(dict_attribute_value["requirement_ID"], "target_value",
+                                                      dict_attribute_value["target_value"])
+            special_fields = [special_field_type, special_field_initial_condition, special_field_test_sequence,
+                              special_field_target_value]
+            # return Requirement object
+            return Requirement(list_parents, None, None, dict_attribute_value["requirement_ID"],
+                               dict_attribute_value["status"], None, dict_attribute_value["traceability"],
+                               dict_attribute_value["objective"], None, None, None, None, special_fields)
 
         if element_type == "technical":
             for attribute in dict_attribute_value:
                 if dict_attribute_value[attribute] == "":
                     if attribute == "requirement_id" or attribute == "relation" or attribute == "technical_description":
                         raise ValueError("Required_attribute_missing")
-            # initialize requirement object and assign values
-            # requirement = Requirement()
+            # initialize special fields
+            special_field_asil = SpecialField(dict_attribute_value["requirement_ID"], "type",
+                                              dict_attribute_value["asil"])
+            special_field_allocation_to_component = SpecialField(dict_attribute_value["requirement_ID"],
+                                                                 "allocation_to_component",
+                                                                 dict_attribute_value["initial_condition"])
+            special_field_target_value = SpecialField(dict_attribute_value["requirement_ID"],
+                                                      "target_value",
+                                                      dict_attribute_value["target_value"])
+            special_fields = [special_field_asil, special_field_allocation_to_component,
+                              special_field_target_value]
+            # return Requirement object
+            return Requirement(list_parents, None, None, dict_attribute_value["requirement_ID"],
+                               dict_attribute_value["status"], None, None,
+                               dict_attribute_value["technical_description"], None, None, None,
+                               dict_attribute_value["comment"], special_fields)
 
         if element_type == "functional":
             for attribute in dict_attribute_value:
@@ -113,19 +108,13 @@ class SpecObjectParser:
                     if attribute == "requirement_id" or attribute == "functional_description":
                         raise ValueError("Required_attribute_missing")
             # initialize special fields
-            """special_field_type = SpecialField(dict_attribute_value["requirement_ID"], "type",
-                                              dict_attribute_value["type"])
-            special_field_initial_condition = SpecialField(dict_attribute_value["requirement_ID"], "initial_condition",
-                                                           dict_attribute_value["initial_condition"])
-            special_field_test_sequence = SpecialField(dict_attribute_value["requirement_ID"], "test_sequence",
-                                                       dict_attribute_value["test_sequence"])
-            special_field_target_value = SpecialField(dict_attribute_value["requirement_ID"], "target_value",
-                                                      dict_attribute_value["target_value"])"""
-            # initialize requirement object and assign values
-            requirement = Requirement(uid=dict_attribute_value["requirement_ID"],
-                                      status=dict_attribute_value["status"])
-            # requirement.requirement_from_dict(dict_attribute_value, dict_attribute_value["requirement_ID"])
-
-        return Requirement("Type", "UID", "Content", "Status", 'status', 'tags', 'references', 'title', 'body',
-                           'rationale', 'rationale_multiline', 'comments', 'special_fields')
-
+            special_field_asil = SpecialField(dict_attribute_value["requirement_ID"], "type",
+                                              dict_attribute_value["asil"])
+            special_field_allocation = SpecialField(dict_attribute_value["requirement_ID"], "allocation",
+                                                    dict_attribute_value["initial_condition"])
+            special_fields = [special_field_asil, special_field_allocation]
+            # return Requirement object
+            return Requirement(list_parents, None, None, dict_attribute_value["requirement_ID"],
+                               dict_attribute_value["status"], None, None,
+                               dict_attribute_value["functional_description"], None, None, None,
+                               dict_attribute_value["comment"], special_fields)
