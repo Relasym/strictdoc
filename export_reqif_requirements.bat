6a9b4dca1df457f8bf87e8df94c3252c1886c0e4
Barrazal <maxi1010@gmx.at>
2021-11-02 14:50:29 +0100

    update export_reqif_requirements.bat to not ask for delete old files in .\output\reqif\

--- export_reqif_requirements.bat
+++ export_reqif_requirements.bat
@@ -1,3 +1,3 @@
-( xcopy .\docs\reqif\ .\output\reqif\ )
-( xcopy .\tests\unit\strictdoc\import\reqif\ .\output\reqif\ )
+( xcopy .\docs\reqif\ .\output\reqif\ /Y)
+( xcopy .\tests\unit\strictdoc\import\reqif\ .\output\reqif\ /Y)
 ( cmd /k poetry run strictdoc export .\output\reqif --experimental-enable-file-traceability --output-dir output\ )
