998f02be64e9b097f81e2c672e67b4df32ade3c2
dursti96 <86721188+dursti96@users.noreply.github.com>
2021-11-03 12:44:13 +0100

    edit test_mapping.py

--- tests/unit/strictdoc/import/reqif/test_mapping.py
+++ tests/unit/strictdoc/import/reqif/test_mapping.py
@@ -1,9 +1,16 @@
 from xml.etree import ElementTree as etree
 from xml.etree.ElementTree import Element
+
+from strictdoc.backend.dsl.models.requirement import Requirement
+from strictdoc.backend.dsl.models.special_field import SpecialField
 from strictdoc.imports.reqif.reqif_objects.specobjectparser import SpecObjectParser
 
+pytest_plugins = [
+    "test_mapping_fixtures.fixture_functional",
+]
 
-def test_mapping_positive():
+
+def test_mapping_positive(fixture_functional_type_map, fixture_functional_relation_map):
     # 1 create testobject from string
     object_string_pos = r"""<SPEC-OBJECT IDENTIFIER="_eDO24C2IEeyvlO4vtsM_UA" LAST-CHANGE="2021-10-15T11:32:40.205+02:00">
           <VALUES>
@@ -22,7 +29,7 @@ def test_mapping_positive():
                 <ATTRIBUTE-DEFINITION-STRING-REF>_KMVP0C2GEeyvlO4vtsM_UA</ATTRIBUTE-DEFINITION-STRING-REF>
               </DEFINITION>
             </ATTRIBUTE-VALUE-STRING>
-            <ATTRIBUTE-VALUE-STRING THE-VALUE="none">
+            <ATTRIBUTE-VALUE-STRING THE-VALUE="ASIL-A">
               <DEFINITION>
                 <ATTRIBUTE-DEFINITION-STRING-REF>_MZGCUC2GEeyvlO4vtsM_UA</ATTRIBUTE-DEFINITION-STRING-REF>
               </DEFINITION>
@@ -38,18 +45,22 @@ def test_mapping_positive():
           </TYPE>
         </SPEC-OBJECT>"""
 
+    special_field_asil = SpecialField("SR001", "asil", "ASIL-A")
+    special_field_allocation = SpecialField("SR001", "allocation", "Software")
+    special_fields = [special_field_asil, special_field_allocation]
     # create test object
-    test_object = SpecObjectParser(uid="SR001", status="Draft", allocation="Software", asil="none", functional_description="The import function shall import a .reqif file and convert it to an .sdoc file")
-
+    test_object = Requirement("DOCUMENT", None, None, "SR001", "Draft", None, None,
+                              "The import function shall import a .reqif file and convert it to an .sdoc file",
+                              None, None, None, None, special_fields)
     # parse object here
     xml_object = etree.fromstring(object_string_pos, etree.XMLParser())
 
     # 2 test mapping
-    requirement = SpecObjectParser.parse(xml_object)
+    requirement = SpecObjectParser.parse(xml_object, fixture_functional_type_map, fixture_functional_relation_map)
 
     # 3 assert
     assert (requirement.uid == test_object.uid)
     assert (requirement.status == test_object.status)
-    assert (requirement.allocation == test_object.allocation)
-    assert (requirement.asil == test_object.asil)
-    assert (requirement.functional_description == test_object.functional_description)
+    assert (requirement.special_fields[0].field_value == test_object.special_fields[0].field_value)
+    assert (requirement.special_fields[1].field_value == test_object.special_fields[1].field_value)
+    assert (requirement.title == test_object.title)
